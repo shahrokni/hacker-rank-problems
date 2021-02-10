19d8b2777f3b628bfeef7bd70dd873589911847a
Mahmoud Shahrokni <shahrokni.trivia@gmail.com>
2021-02-11 00:00:48 +0330

    02.11.2021

--- permutation-equation.c
+++ permutation-equation.c
@@ -1,14 +1,11 @@
 /*https://www.hackerrank.com/challenges/permutation-equation/problem*/
 int * permutationEquation(int p_count, int * p, int * result_count) {
-
   * result_count = p_count;
-  int y_arr[p_count + 1];
+  static int y_arr[51];
   for (int i = 0; i < p_count; i++) {
     int x = * (p + * (p + i) - 1);
-    y_arr[x] = i + 1;
+    printf("x = %d p(p(%d)) = %d \n", x, i, *(p + * (p + i) - 1));
+    y_arr[x - 1] = i + 1;
   }
-  for (int i = 1; i <= p_count; i++) {
-    printf("%d\n", y_arr[i]);
-  }
-  return result_count;
+  return y_arr;
 }
